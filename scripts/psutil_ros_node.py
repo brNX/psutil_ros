31d39e328e744185a03bb2789c4ffe0e67ae4fc4
Bruno Gouveia <bgouveia@gmail.com>
2013-12-17 13:50:26 +0000

    forgotten files

--- scripts/psutil_ros_node.py
+++ scripts/psutil_ros_node.py
@@ -2,41 +2,70 @@
 import psutil
 import rospy
 
-from psutil_ros.msg import Sysinfo
+from psutil_ros.msg import *
 from std_msgs.msg import Time
 
 
 
-def pusblisher():
-    pub = rospy.Publisher('psutil_ros', Sysinfo)
+def publisher():
+    
+    cpu_load_pub = rospy.Publisher('cpu_load', Cpuload)
+    meminfo_pub = rospy.Publisher('meminfo', Meminfo)
+    network_pub = rospy.Publisher('network', Network)
+   
     rospy.init_node('psutil_ros')
+    publish_rate = rospy.get_param("~publish_rate",1);   
+    r = rospy.Rate(publish_rate)
+    
     while not rospy.is_shutdown():
         #str = "hello world %s" % rospy.get_time()
-        sys_info = Sysinfo()
+        time = rospy.Time.now()
         
-        sys_info.stamp = rospy.Time.now()
-        
+        cpuloadmsg = Cpuload()
+        cpuloadmsg.stamp = time
         loads = psutil.cpu_percent(interval=0,percpu=True)
+        cpuloadmsg.cpu_load=loads
         
+        meminfomsg = Meminfo()
+        meminfomsg.stamp = time
         meminfo=psutil.virtual_memory()
-        sys_info.memtotal = meminfo.total
-        sys_info.memavailable = meminfo.available
-        sys_info.mempercent = meminfo.percent
-        sys_info.memused = meminfo.used
-        sys_info.memfree = meminfo.free
-        sys_info.memactive = meminfo.active
-        sys_info.meminactive = meminfo.inactive
-        sys_info.membuffers = meminfo.buffers
-
-        sys_info.cpu_load=loads
+        meminfomsg.memtotal = meminfo.total
+        meminfomsg.memavailable = meminfo.available
+        meminfomsg.mempercent = meminfo.percent
+        meminfomsg.memused = meminfo.used
+        meminfomsg.memfree = meminfo.free
+        meminfomsg.memactive = meminfo.active
+        meminfomsg.meminactive = meminfo.inactive
+        meminfomsg.membuffers = meminfo.buffers
+        
+        networkmsg = Network()
+        networkmsg.stamp=time
+        
+        interfaces = psutil.net_io_counters(pernic=True)
+        
+        for interface in interfaces:
+            interfacemsg=Interface()
+            interfacemsg.name=interface
+            interfacemsg.bytes_sent=interfaces[interface].bytes_sent
+            interfacemsg.bytes_recv=interfaces[interface].bytes_recv
+            interfacemsg.packets_sent=interfaces[interface].packets_sent
+            interfacemsg.packets_recv=interfaces[interface].packets_recv
+            interfacemsg.errin=interfaces[interface].errin
+            interfacemsg.errout=interfaces[interface].errout
+            interfacemsg.dropin=interfaces[interface].dropin
+            interfacemsg.dropout=interfaces[interface].dropout
+            networkmsg.interfaces.append(interfacemsg)
+        
         
         #rospy.loginfo(sys_info)
-        pub.publish(sys_info)
-        rospy.sleep(1.0)
+        cpu_load_pub.publish(cpuloadmsg)
+        meminfo_pub.publish(meminfomsg)
+        network_pub.publish(networkmsg)
+        r.sleep()
 
 
 if __name__ == '__main__':
     try:
-        pusblisher()
+        publisher()
     except rospy.ROSInterruptException:
         pass
